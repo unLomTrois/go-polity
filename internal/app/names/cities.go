23d7b1cd98c095991ac5f68292b1e8178996ad65
unlomtrois <vadim.kuz02@gmail.com>
2022-12-26 03:13:45 +0300

    generate names within shuffled names

--- internal/app/names/cities.go
+++ internal/app/names/cities.go
@@ -1,5 +1,14 @@
 package names
 
+import (
+	"math/rand"
+)
+
+var names = []string{"Ur", "Uruk", "lagash", "nippur", "akkad", "Wakad", "Kad", "Kadu", "Lilum"}
+
 func GenerateName() string {
-	return "Ur"
+	rand.Shuffle(len(names), func(i, j int) {
+		names[i], names[j] = names[j], names[i]
+	})
+	return names[rand.Intn(len(names))]
 }
