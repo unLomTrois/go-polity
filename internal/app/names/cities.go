a685f0a49ec4b695d70f6ff56386f1a5fb4a692f
unlomtrois <vadim.kuz02@gmail.com>
2022-12-26 03:21:33 +0300

    add generating names based on initial and final

--- internal/app/names/cities.go
+++ internal/app/names/cities.go
@@ -4,11 +4,15 @@ import (
 	"math/rand"
 )
 
-var names = []string{"Ur", "Uruk", "lagash", "nippur", "akkad", "Wakad", "Kad", "Kadu", "Lilum"}
+var initial = []string{"Ur", "La", "Ak", "Nip", "Ak", "Wa", "Ka", "Ba", "Kab", "Li"}
+var final = []string{"", "uk", "shak", "pur", "kad", "du", "lum"}
 
 func GenerateName() string {
-	rand.Shuffle(len(names), func(i, j int) {
-		names[i], names[j] = names[j], names[i]
+	rand.Shuffle(len(initial), func(i, j int) {
+		initial[i], initial[j] = initial[j], initial[i]
 	})
-	return names[rand.Intn(len(names))]
+	rand.Shuffle(len(final), func(i, j int) {
+		final[i], final[j] = final[j], final[i]
+	})
+	return initial[rand.Intn(len(initial))] + final[rand.Intn(len(final))]
 }
